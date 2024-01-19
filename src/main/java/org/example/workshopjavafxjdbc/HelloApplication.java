5900478823a651604c9153dcae93a9407a12ed6b
thiagofmj <thiagofmj16@outlook.com>
2024-01-19 10:57:03 -0300

    Main view design

--- src/main/java/org/example/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/org/example/workshopjavafxjdbc/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.workshopjavafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene mainScene = new Scene(scrollPane);
+        stage.setScene(mainScene);
         stage.show();
     }
 
