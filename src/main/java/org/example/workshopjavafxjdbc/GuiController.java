0a61cfd96dd746035c3393c942d8f32dd9120a61
thiagofmj <thiagofmj16@outlook.com>
2024-01-19 11:22:49 -0300

    Main view Controller

---
+++ src/main/java/org/example/workshopjavafxjdbc/GuiController.java
@@ -0,0 +1,35 @@
+package org.example.workshopjavafxjdbc;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuiController implements Initializable {
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem manuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSllerAction");
+    }
+    public void onMenuItemDepartementAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/main/java/org/example/workshopjavafxjdbc/HelloController.java
+++
@@ -1,8 +0,0 @@
-package org.example.workshopjavafxjdbc;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-
-}
